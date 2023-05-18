e740f681153b66b35f0871cfbe48b56aba8eeb2c
Patrick Pereira <patrick.pereira27@hotmail.com>
2023-05-18 15:01:52 -0300

    Criação de modelo

--- src/main/java/loja_virtual/LojaVirtualApplication.java
+++ src/main/java/loja_virtual/LojaVirtualApplication.java
@@ -2,12 +2,15 @@ package loja_virtual;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "loja_virtual_model")
 public class LojaVirtualApplication {
 
 	public static void main(String[] args) {
-		System.out.println("teste");
+		// chave para commit 
+		//----->    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
 		SpringApplication.run(LojaVirtualApplication.class, args);
 	}
 
